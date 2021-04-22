496b9b07c49861dcdff30252e484bf7579ef0188
Alex Waese-Perlman <squishybanana04@gmail.com>
2021-04-22 12:16:26 -0400

    finish 4D

--- codeforces/4/4D.cpp
+++ codeforces/4/4D.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
 #include <map>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int dp[5001][5001];
-pair<int, int> sizes[5001];
+vector<int> dp;
+vector<pair<int, int>> sizes;
+vector<int> path;
+
+int stackSize(int idx) {
+	if (dp[idx]) return dp[idx];
+	for (int i = 0; i < sizes.size(); i++) {
+		if (sizes[i].first > sizes[idx].first && sizes[i].second > sizes[idx].second) {
+			int size = stackSize(i) + 1;
+			if (size > dp[idx]) {
+				dp[idx] = size;
+				path[idx] = i;
+			}
+		}
+	}
+	return dp[idx];
+}
 
 void solve() {
 	int n, w, h;
 	cin >> n >> w >> h;
 	
+	dp.resize(n + 1);
+	path.resize(n + 1);
+	sizes.emplace_back(w, h);
 	for (int i = 0; i < n; i++) {
-		cin >> sizes[i].first >> sizes[i].second;
+		cin >> w >> h;
+		sizes.emplace_back(w, h);
 	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			
-		}
+	
+	cout << stackSize(0) << endl;
+	int p = 0;
+	while (path[p]) {
+		cout << path[p] << " ";
+		p = path[p];
 	}
-
+	cout << endl;
 }
 
 int main() {
