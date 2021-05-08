62d7d40e67cc496ebaada99afb7158fd32c93629
Alex Waese-Perlman <squishybanana04@gmail.com>
2021-05-07 22:40:16 -0400

    modifications to 1515

--- codeforces/1515/1515B.cpp
+++ codeforces/1515/1515B.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 using namespace std;
-int isqrt(int n) {
+using ll = long long;
+
+ll isqrt(ll n) {
 	if (n == 1) return 1;
-	int lb = 0, ub = n;
+	ll lb = 0, ub = n;
 	while (ub - lb > 1) {
-		int s = (lb + ub) / 2;
+		ll s = (lb + ub) / 2;
 		if (s * s == n) return s;
 		else if (n > s * s) lb = s;
 		else if (n < s * s) ub = s;
@@ -13,16 +15,16 @@ int isqrt(int n) {
 	return lb;
 }
 
-bool psquare(int n) {
-	int s = isqrt(n);
+bool psquare(ll n) {
+	ll s = isqrt(n);
 	return s * s == n;
 }
 
 
 int main() {
-	int t, n;
+	ll t, n;
 	cin >> t;
-	for (int i = 0; i < t; i++) {
+	for (ll i = 0; i < t; i++) {
 		cin >> n;
 		if ((n % 2 == 0  && psquare(n / 2)) || (n % 4 == 0 && psquare(n / 4))) cout << "YES" << endl;
 		else cout << "NO" << endl;
--- codeforces/1515/1515D.cpp
+++ codeforces/1515/1515D.cpp
@@ -8,35 +8,16 @@
 using namespace std;
 
 int L[200005], R[200005];
-int removeMatching(int &l, int &r, unordered_set<int> &t) {
-	int matched = 0;
-	stack<pair<int, int>> matching;
-	for (auto i : t) {
-		if (L[i] > 0 && R[i] > 0) {
-			matching.emplace(i, min(L[i], R[i]));
-		}
-	}
-	while (!matching.empty()) {
-		auto top = matching.top();
-		//cout << top.first << " " << top.second << endl;
-		matching.pop();
-		L[top.first] -= top.second;
-		R[top.first] -= top.second;
-		l -= top.second;
-		r -= top.second;
-		matched += top.second;
-	}
-	
-	return matched;
-}
 
 int solve() {
 	int n, l, r;
 	cin >> n >> l >> r;
 
+
 	unordered_set<int> t;
 
 	for (int i = 0; i <= n; i++) L[i] = 0, R[i] = 0;
+	
 	for (int i = 0; i < n; i++) {
 		int c; cin >> c;
 		if (i < l) L[c]++;
@@ -44,42 +25,32 @@ int solve() {
 		t.insert(c);
 	}
 
-	// remove already matching pairs
-	int matched = 0;
-	matched += removeMatching(l, r, t);
-	if (matched >= n / 2) return 0;
-
-	int cost = 0;
-
-	// move duplicates over until the length is the same
 	for (int i : t) {
-		int diff = min(abs(L[i] - R[i]) / 2, abs(l - r));
-		//cout << "type: " << i << " diff: " << diff << endl;
-		if (L[i] > 0) {
-			R[i] = diff;
-			r += diff;
-			L[i] -= diff;
-			l -= diff;
-			cost += diff;
-		}
-		else if (R[i] > 0) {
-			R[i] -= diff;
-			r -= diff;
-			L[i] = diff;
-			l += diff;
-			cost += diff;
+		if (L[i] > 0 && R[i] > 0) {
+			int m = min(L[i], R[i]);
+			L[i] -= m;
+			l -= m;
+			R[i] -= m;
+			r -= m;
 		}
 	}
 
-	matched += removeMatching(l, r, t);
-
-/*	cout << "L: ";
-	for (auto i : t) cout << i << ": " <<L[i] << ", ";
-	cout << endl;
-	cout << "R: ";
-	for (auto i : t) cout << i << ": " << R[i] << ", ";
-	cout << endl;
-	cout << l << " " << r << " " << cost << endl;*/
+	int cost = 0;
+	for (int i : t) {
+		int *larger;
+		if (R[i] > 1) larger = &R[i];
+		else larger = &L[i];
+		
+		int c = min((*larger / 2) * 2, R[i] > 1 ? r - l : l - r);
+		if (c > 1) {
+			//cout << "C: " << c << ", larger: " << *larger << endl;
+			cost += c / 2;
+			*larger -= c;
+			if (larger == &R[i]) r -= c;
+			else l -= c;
+		}
+	}
+	//cout << l << " " << r << endl;
 
 	return cost + abs(l - r) / 2 + (l + r) / 2;
 }
