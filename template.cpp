7dae33ef96395455f43df10013d0b4d1238ecd1e
Alex Waese-Perlman <squishybanana04@gmail.com>
2021-12-22 14:03:39 -0500

    fix ordered_set in template

--- template.cpp
+++ template.cpp
@@ -11,13 +11,7 @@ using namespace __gnu_pbds;
 #define print_array(x) (cout << #x); for (auto i : (x)) cout << " " << i; cout << endl;
 
 template<typename T>
-using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-ull gcd(ull a, ull b) {
-	if (b > a) return gcd(a, b);
-	if (b == 0) return a;
-	return gcd(b, a%b);
-}
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 int main() {
 	ios_base::sync_with_stdio(false);
